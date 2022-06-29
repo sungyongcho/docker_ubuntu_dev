813ee05557a1320216d278f266ca7eba697029d8
Sungyong Cho <to.sungyongcho@gmail.com>
2022-06-29 04:12:00 +0200

    added ssh keygen

--- create_container.sh
+++ create_container.sh
@@ -11,7 +11,10 @@ docker run -it -d --name ubuntu-docker --hostname ubuntu-docker \
 docker exec -it ubuntu-docker apt -y update
 docker exec -it ubuntu-docker apt -y upgrade
 
-docker exec -it ubuntu-docker apt install -y systemd zsh gcc git
+docker exec -it ubuntu-docker apt install -y zsh gcc git
 
 docker exec -it ubuntu-docker chsh -s /usr/bin/zsh
 
+docker exec -it ubuntu-docker ssh-keygen -t rsa -C "to.sungyongcho@gmail.com" -f "/root/.ssh/id_rsa_github"
+docker exec -it ubuntu-docker sh -c 'echo "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/id_rsa_github" > /root/.ssh/config'
+docker exec -it ubuntu-docker cat /root/.ssh/id_rsa_github.pub
